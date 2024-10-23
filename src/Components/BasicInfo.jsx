3d57e59103ef3a8a03cc61ad8acedfd9b53d8842
asgharali97 <asgharalifs48@gmail.com>
2024-10-22 23:06:03 -0700

    add local storage and errors handling

--- src/Components/BasicInfo.jsx
+++ src/Components/BasicInfo.jsx
@@ -1,29 +1,32 @@
-import React,{useState} from "react";
+import React,{useEffect, useState} from "react";
 import Input from "./Input";
 import { useDispatch,useSelector } from "react-redux";
-import {Link} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import {setBasicInfo} from '../store/UserSlice'
 import {useForm} from 'react-hook-form';
 import Button from "./button";
 const BasicInfo = () => {
-  const [error , setError] = useState('');
-   const dispatch = useDispatch();
-   const {register , handleSubmit} = useForm();
-   const info = useSelector((state)=> state.resume.basicInfo)
-   console.log(info)
+  const dispatch = useDispatch();
+  const {register , handleSubmit,setValue , formState: { errors }} = useForm();
+  const navigate = useNavigate();
+
+   useEffect(()=>{
+    const savedData = localStorage.getItem('basicInfo');
+    if(savedData){
+      const parsedData = JSON.parse(savedData);
+      Object.keys(parsedData).forEach((key) => setValue(key, parsedData[key]));
+      dispatch(setBasicInfo(parsedData))
+    }
+   },[dispatch,setValue])
 
    const onSubmit = (data)=>{
-    try {
-      setError
-      if(data){
+      if(Object.keys(errors).length === 0){
         dispatch(setBasicInfo(data))
         console.log(data)
-      }
-    } catch (error) {
-      setError(error.message)
-    }
+        localStorage.setItem('basicInfo', JSON.stringify(data)); 
+        navigate('/summary'); 
    }
-
+   }
  
   return (
     <>
@@ -32,63 +35,62 @@ const BasicInfo = () => {
           <h1 className="text-3xl font-semibold mb-4">Basic Info</h1>
         </div>
         <div className="w-[70vw] h-[60vh]">
-        {error && <p className="text-red-600 mt-8 text-center">{error}p</p>}
-          <form onSubmit={handleSubmit(onSubmit,error)} className="flex flex-wrap gap-8">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-wrap gap-8 relative">
             <Input
               label="Name"
-              placeholder='Enter your Full Name'
-              {...register("name",{
-                required:true
-              })}
+              placeholder="Enter your Full Name"
+              className={`${errors.name ? 'mt-5' : ''}`}
+              {...register("name", { required: "Name is required" })}
             />
+            {errors.name && <p className="text-red-600 absolute top-[4vh] px-1">{errors.name.message}</p>}
             <Input
               label="Email"
-              placeholder='Enter your Email'
-              {...register("email",{
-                required:true,
-                validate: {
-                  matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
-                  "Email address must be a valid address",
-              }
+              placeholder="Enter your Email"
+              className={`${errors.email ? 'mt-5' : ''}`}
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+                  message: "Email address must be valid"
+                }
               })}
             />
+            {errors.email && <p className="text-red-600 absolute top-[4vh] left-[32.5vw] px-1">{errors.email.message}</p>}
             <Input
               label="Phone No"
-              placeholder='Enter your Phone Number'
+              placeholder="Enter your Phone Number"
+              className={`${errors.phone ? 'mt-5' : ''}`}
               type="number"
-              {...register("phone",{
-                required:true,
-              })}
+              {...register("phone", { required: "Phone number is required" })}
             />
+            {errors.phone && <p className="text-red-600  absolute top-[25vh] px-1">{errors.phone.message}</p>}
             <Input
               label="LinkedIn Link"
-              placeholder='Enter your LinkedIn profile link'
-              {...register("linkedIn",{
-                validate: {
-                  matchPatern: (value) => /^(?:(?:https?|ftp):\/\/)?(?:www\.)?[a-z0-9-]+(?:\.[a-z0-9-]+)+[^\s]*$/i.test(value) ||
-                  "Url must be valid",
-              }
+              placeholder="Enter your LinkedIn profile link"
+              className={`${errors.linkedIn ? 'mt-5' : ''}`}
+              {...register("linkedIn", {
+                pattern: {
+                  value: /^(?:(?:https?|ftp):\/\/)?(?:www\.)?[a-z0-9-]+(?:\.[a-z0-9-]+)+[^\s]*$/i,
+                  message: "LinkedIn URL must be valid"
+                }
               })}
             />
+            {errors.linkedIn && <p className="text-red-600 absolute top-[22vh] left-[32.5vw]">{errors.linkedIn.message}</p>}
             <Input
               label="GitHub Link"
-              placeholder='Enter your GitHub profile link'
-              {...register("github",{
-                validate: {
-                  matchPatern: (value) => /^(?:(?:https?|ftp):\/\/)?(?:www\.)?[a-z0-9-]+(?:\.[a-z0-9-]+)+[^\s]*$/i.test(value) ||
-                  "Url must be valid",
-              }
+              placeholder="Enter your GitHub profile link"
+              className={`${errors.github ? 'mt-5' : ''}`}
+              {...register("github", {
+                pattern: {
+                  value: /^(?:(?:https?|ftp):\/\/)?(?:www\.)?[a-z0-9-]+(?:\.[a-z0-9-]+)+[^\s]*$/i,
+                  message: "GitHub URL must be valid"
+                }
               })}
             />
-            
-            {/* <Link to='/summary'> */}
-            <Button 
-            type="submit"
-            className="mt-7"
-            >
-            add
+            {errors.github && <p className="text-red-600 absolute top-[40vh] px-1">{errors.github.message}</p>}
+            <Button type="submit" className="mt-7">
+              Add
             </Button>
-            {/* </Link> */}
           </form>
         </div>
       </div>
--- src/Components/Summary.jsx
+++ src/Components/Summary.jsx
@@ -1,13 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Input from "./Input";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setSummary } from "../store/UserSlice";
+import { useForm } from "react-hook-form";
+import Button from "./button";
 
 const Summary = () => {
+  const dispatch = useDispatch();
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const savedData = localStorage.getItem("summary");
+    if (savedData) {
+      const parsedData = JSON.parse(savedData);
+      Object.keys(parsedData).forEach((key) => setValue(key, parsedData[key]));
+      dispatch(setSummary(parsedData));
+    }
+  }, [dispatch, setValue]);
+
+  const onSubmit = (data) => {
+    if (Object.keys(errors).length === 0) {
+      dispatch(setSummary(data));
+      console.log(data);
+      localStorage.setItem("summary", JSON.stringify(data));
+      navigate("/skills");
+    }
+  };
   return (
     <>
-      <div className="w-full">
-         
+      <div className="w-full h-screen bg-[#121212] py-8 px-16">
+        <div className="head py-4 ">
+          <h1 className="text-3xl font-semibold mb-4">Basic Info</h1>
+        </div>
+        <div className="w-[70vw] h-[60vh]">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-wrap gap-8 relative"
+          >
+            <Input
+              label="Professional Title"
+              placeholder="e.g Full Stack Developer , Data Scientist"
+              className={`${errors.title ? "mt-5" : ""}`}
+              {...register("title", { required: "Title is required" })}
+            />
+            {errors.title && (
+              <p className="text-red-600 absolute top-[7vh] px-1">
+                {errors.title.message}
+              </p>
+            )}
+            <Input
+              label="Describe yourself"
+              placeholder="Describe yourself in one or two line"
+              className={`${errors.summary ? "mt-5" : ""}`}
+              {...register("summary", {
+                required: "summary is required",
+              })}
+            />
+            {errors.summary && (
+              <p className="text-red-600 absolute top-[7vh] left-[32.5vw] px-1">
+                {errors.summary.message}
+              </p>
+            )}
+            <Button type="submit" className="mt-4 absolute top-32 left-[54vw]">
+              Add
+            </Button>
+          </form>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Summary
+export default Summary;
